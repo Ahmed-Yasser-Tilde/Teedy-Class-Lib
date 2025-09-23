72b2219a6359c4d3d293878af2db6589cf49fed0
Ahmed <ahmed.yasser@tildetech.ae>
2025-09-23 16:03:39 +0300

    Enhance and Validate the logic and test

---
+++ Teedy.CL/Services/DatabaseService/DbHelper.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Teedy.CL.Services.DatabaseService
+{
+    public class DbHelper
+    {
+        private readonly string _connectionString;
+
+        public DbHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<T> Query<T>(string sql, object parameters = null)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                return db.Query<T>(sql, parameters);
+            }
+        }
+
+        public int Execute(string sql, object parameters = null)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                return db.Execute(sql, parameters);
+            }
+        }
+    }
+}
